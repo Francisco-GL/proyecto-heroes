882587e0deddcda0e7000b34c12649d703224ca3
Francisco-2103 <al270032@edu.uaa.mx>
2022-05-08 22:30:15 -0500

    Incorporacion 6 objectos al array Heroe

--- src/app/misheroes.ts
+++ src/app/misheroes.ts
@@ -49,5 +49,47 @@ export const HEROES:Heroe[]=[
         img: "assets/img/wolverine.png",
         aparicion: "1974-11-01",
         casa: "Marvel"
+      },
+      {
+        nombre: "Iron Man",
+        bio: "Anthony Edward Stark, conocido como Tony Stark, es un multimillonario magnate empresarial estadounidense, playboy e ingenioso científico, ficticio del universo Marvel. Más allá de los poderes que le concede el traje, Tony es mucho más que un prodigio de la ingeniería mecánica que se graduó del Instituto de Tecnología de Massachusetts con honores a la edad de 17 años. Tony puede construir y reparar su armadura incluso modificándola para un mejor uso.",
+        img: "assets/img/ironman.png",
+        aparicion: "1968-05-01",
+        casa: "Marvel"
+      },
+      {
+        nombre: "Capitan America",
+        bio: "El Capitán América, cuyo nombre real es Steven Grant Rogers, es un superhéroe ficticio del universo Marvel. Es remarcablemente fuerte, rápido y resistente pero en ningún caso esas capacidades pueden considerarse superhumanas. También ha demostrado ser un consumado estratega con grandes dotes de liderazgo, y ha terminado dirigiendo casi cualquier organización o grupo en el que se ha visto involucrado.",
+        img: "assets/img/capitan-america.png",
+        aparicion: "1941-03-01",
+        casa: "Marvel"
+      },
+      {
+        nombre: "Deadpool",
+        bio: "Wade Winston Wilson, más conocido como Deadpool, ficticio del universo Marvel, al igual que sucede con Wolverine, la personalidad y habilidades de Deadpool son en gran medida resultado de haber sido sometido al programa paramilitar del gobierno canadiense conocido como Arma X. Después de que Arma X curara su cáncer terminal implantando el (factor curativo) extraído de Wolverine, Deadpool quedó desfigurado y mentalmente inestable.",
+        img: "assets/img/deadpool.png",
+        aparicion: "1991-02-01",
+        casa: "Marvel"
+      },
+      {
+        nombre: "Mujer Maravilla",
+        bio: "La Mujer Maravilla o Wonder Woman, tal es su denominación original en el idioma inglés, es una superheroína que fuera creada en la década del cuarenta del siglo pasado por el autor William Moulton Marston, en el marco de su trabajo como historietista para la editorial DC Comics.",
+        img: "assets/img/mujer-maravilla.png",
+        aparicion: "1941-12-01",
+        casa: "DC"
+      },
+      {
+        nombre: "Flash",
+        bio: "Flash (también conocido por su apodo (El Velocista Escarlata) es un superhéroe ficticio del universo DC Comics que posee una rapidez sobrehumana, la cual incluye la habilidad de correr a gran velocidad, reflejos sobrehumanos y la capacidad de violar algunas leyes de la física.",
+        img: "assets/img/flash.png",
+        aparicion: "1940-01-01",
+        casa: "DC"
+      },
+      {
+        nombre: "Robin",
+        bio: "Robin es un personaje ficticio de DC Comics, es un joven superhéroe compañero de Batman. Desde 1940, varios personajes han ocupado el rol de Robin, entre ellos, uno de los más conocidos ha sido el de Dick Grayson. Todas las versiones de Robin han sido las de un chico joven con traje colorido, bastante positivo y alegre, lo cual contrasta perfectamente con la apariencia oscura y personalidad seria de Batman.",
+        img: "assets/img/robin.png",
+        aparicion: "1940-04-01",
+        casa: "DC"
       }
 ];
